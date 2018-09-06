b7d8ff8224fe3156e45c130737f19a1cbf61f8e0
May <1137112460@qq.com>
2018-09-06 17:07:27 +1000

    NYU

--- AlgorithmProblemSolving/rec/ans/1/flagstones.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int n, m, a;
-int main() {
-  while (cin >> n >> m >> a) {
-      cout << 1ll * ((n + a - 1) / a) * ((m + a - 1) / a) << endl;
-  }
-  return 0;
-}
